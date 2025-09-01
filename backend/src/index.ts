e7fb484c34044d0cf796dcb941bb5a2f43320299
FredricLaestander <fredriclaestander@gmail.com>
2025-09-01 11:51:22 +0200

    add express backend

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import type { Request, Response } from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`server ready on http://localhost:${process.env.PORT}`);
+});
+
+export default app;
