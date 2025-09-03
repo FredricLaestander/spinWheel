6a2557c6bcc7c44a6ccf6279e883c7a6f48eb790
obeyhades <abdu.nazar145@gmail.com>
2025-09-03 15:06:42 +0200

    feat: implomented spinRoute to index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import type { Request, Response } from "express";
 import connectDB from "./database/db";
 import router from "./routes/userRoute";
+import spinRouter from "./routes/spinRoute";
+
 
 dotenv.config();
 
@@ -10,6 +12,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use("/",spinRouter)
 app.use ("/", router)
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
